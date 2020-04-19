357a0f496c8981f434ece58c2c4babc964470623
Jong <kau96kim@gmail.com>
2020-04-19 20:24:33 +0900

    props를 통해 컴포넌트에게 값 전달하기

--- src/Hello.js
+++ src/Hello.js
@@ -1,23 +1,20 @@
 import React from 'react';
 import './App.css';
 
-function HelloWorld() {
+function HelloWorld( {color, name} ) {
     const style = {
-        backgroundColor: 'gray',
-        color: 'aqua',
-        padding: '1rem',
-        fontSize: 24
+        color
     }
     return (
-    <>
-        {/* 주석입니다 */}
-        <div>태그는 꼭 닫혀있어야 함</div>
-        <div>두개 이상의 태그는 하나의 태그로 묶여있어야 함</div>
-        <div style={style}>Hello, world!</div>
-        <br />
-        <div className="gray-box"></div>
-    </>
+        <>
+            <div style={style}>Hello, world! name: {name}</div>
+        </>
     );
+}
+
+HelloWorld.defaultProps = {
+    name: "이름 없음",
+    color: "aqua"
 }
 
 export default HelloWorld;
