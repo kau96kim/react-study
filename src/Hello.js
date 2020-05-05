98a87c6f59f6e65c3e7d998be0fe819b59c92db4
Jong <kau96kim@gmail.com>
2020-05-05 20:10:40 +0900

    클래스형 컴포넌트 소개

--- src/Hello.js
+++ src/Hello.js
@@ -1,23 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function HelloWorld( {color, name, isSpecial} ) {
-  const style = {
-    color
-  }
-  return (
-    <div style={style}>
-      {isSpecial ? <b>* </b> : null}
-      Hello, world! name: {name}
-      {isSpecial && <b> *</b>}
-    </div>
-  );
-}
 
-HelloWorld.defaultProps = {
-  name: "이름 없음",
-  color: "aqua",
-  isSpecial: false
+class HelloWorld extends Component {
+  static defaultProps = {
+    name: "이름없음",
+    color: "aqua",
+    isSpecial: false
+  };
+
+  render() {
+    const { color, name, isSpecial } = this.props;
+    return (
+      <div style={{color}}>
+        {isSpecial ? <b>* </b> : null}
+        Hello, world! name: {name}
+        {isSpecial && <b> *</b>}
+      </div>
+    );
+  };
 }
 
 export default HelloWorld;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import HelloWorld from './Hello';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <HelloWorld />
   </React.StrictMode>,
   document.getElementById('root')
 );
